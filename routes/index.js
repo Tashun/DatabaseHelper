6fdca7754877e129c50cf83db1a29865b421c3d4
Tashun <ttashun.15@cse.mrt.ac.lk>
2018-04-18 19:02:44 +0530

    after basic 2nd app

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var FoodItem=require('../models/foodItem');
+var InventoryItem=require('../models/inventoryItem');
 var router = express.Router();
 var mongoose=require("mongoose");
 
@@ -16,6 +17,34 @@ router.get('/FoodItems',function (req,res,next) {
  });
 });
 
+//add a new Item to intentory
+router.post('/addInventory',function (request,response) {
+    var inventoryItem= new InventoryItem({
+        name:request.body.name,
+        quantity:request.body.quantity,
+        stockMin:request.body.stockMin
+
+    });
+   inventoryItem.save(function (err) {
+       if (err){
+           throw err;
+       }
+       else {
+           response.send({validity:true});
+       }
+   });
+});
+
+//update a item in inventory
+router.post('/updateInventory',function (request,response) {
+    InventoryItem.count( {name:request.body.name}, function (err, count){
+        if (count>0){
+            response.send({validity:true});
+        }
+    });
+
+});
 
 
 module.exports = router;
+
